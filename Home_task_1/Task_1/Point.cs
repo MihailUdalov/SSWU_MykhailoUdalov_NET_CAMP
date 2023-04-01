c5455f54fa45c54124020e8294bb456da5a1e98f
Удалов Михаил <misha2001dw@gmail.com>
2023-04-01 22:23:36 +0300

    Change matrix for myself

--- Home_task_1/Task_1/Point.cs
+++ Home_task_1/Task_1/Point.cs
@@ -2,7 +2,7 @@
 {
     internal class Point
     {
-        public Direction Direction { get;set; }
+        public Direction Direction { get; set; }
         public int Value { get; private set; }
 
         public Point(Direction direction,int value)
