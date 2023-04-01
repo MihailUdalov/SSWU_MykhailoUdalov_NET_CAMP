cd15152b02b7ceb0fc1f6fe53c7bfe074dabe29b
senivlm <91743906+senivlm@users.noreply.github.com>
2023-04-01 21:41:43 +0300

    Update Matrix.cs

--- Home_task_1/Task_1/Matrix.cs
+++ Home_task_1/Task_1/Matrix.cs
@@ -4,7 +4,8 @@
     {
         public int Width { get; private set; }
         public int Height { get; private set; }
-
+// Порушення інкапсуляції. Ви повертаєте посилання, через яке можна доступитись до поля.
+        
         public Point[,] Cells { get; private set; }
 
         public Matrix(int width, int height)
@@ -35,7 +36,7 @@
             for (int col = 0, row = 0, matrixLength = 1; matrixLength < Height * Width;)
             {
                 switch (direction)
-                {
+                {// Завелика кількість умов на кожному кроці.
                     case Direction.Right:
                         if (col == rightLimit || Cells[row, col + 1] != null)
                         {
