a4ec3bb647a1bb2be5b7a854e90d83cde217d2cd
Удалов Михаил <misha2001dw@gmail.com>
2023-04-03 22:57:49 +0300

    Done in task 3, sub task 2

---
+++ Home_task_3/Task_2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Write your text:");
+            string text = Console.ReadLine();
+
+            Console.Write("Write your substring in text:");
+            string substring = Console.ReadLine();
+
+            // Task a
+            Console.WriteLine("Task A");
+
+            int? index = TextHandler.FindOccurrence(text, substring);
+            if (index != null)
+                Console.WriteLine("The index of second occurrence of the substring '{0}': {1} \n", substring, index);
+            else
+                Console.WriteLine("No entry");
+
+            // Task b
+            Console.WriteLine("Task B");
+
+            Console.WriteLine("Number of words starting with a capital letter: {0}", TextHandler.CountBigLetter(text));
+
+            // Task c
+            Console.WriteLine("Task C");
+
+            Console.Write("Write a replacement for a word with repeated letters:");
+            string replacementText = Console.ReadLine();
+
+            Console.WriteLine("Text with replaced words: {0}", TextHandler.ReplaceWords(text, replacementText));
+
+            Console.WriteLine("Press any button to close program");
+            Console.ReadKey();
+        }
+
+        
+
+    }
+}
---
+++ Home_task_3/Task_2/TextHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    static class TextHandler
+    {
+        public static int? FindOccurrence(string text, string substring)
+        {
+            int firstIndex = text.IndexOf(substring);
+            if (firstIndex >= 0)
+            {
+                firstIndex = text.IndexOf(substring, firstIndex + 1);
+                if (firstIndex == -1)
+                    return null;
+                return firstIndex;
+            }
+            return null;
+        }
+
+        public static int CountBigLetter(string text)
+        {
+            return text.Split().Count(word => char.IsUpper(word[0]));
+        }
+
+        public static string ReplaceWords(string text, string replacement)
+        {
+            Regex regex = new Regex(@"\b\w*(\w)\1\w*\b");
+            return regex.Replace(text, replacement);
+        }
+    }
+}
