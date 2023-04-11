d177af6791e02002143702413fa595b87014c515
Удалов Михаил <misha2001dw@gmail.com>
2023-04-11 18:31:43 +0300

    Change regex

--- Home_task_3/Task_2/TextHandler.cs
+++ Home_task_3/Task_2/TextHandler.cs
@@ -29,7 +29,7 @@ namespace Task_2
 
         public static string ReplaceWords(string text, string replacement)
         {// помилка в регулярці
-            Regex regex = new Regex(@"\b\w*(\w)\1\w*\b");
+            Regex regex = new Regex(@"\b\w*(\w)\1\w*\b",RegexOptions.IgnoreCase);
             return regex.Replace(text, replacement);
         }
     }
